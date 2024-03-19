14144a75f10f7b01e2a56da192bf2f592327dda1
emamoldo <moldovanemanuel92.em@gmail.com>
2024-03-19 13:29:59 +0100

    Add the Structure in Js for the email generation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
 console.log('works');
+const { createApp } = Vue 
+createApp({
+    data() {
+        return {
+            randomEmails: null
+        }
+    },
+    methods: {
+        callApi() {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => {
+                console.log(response.data.response);
+                this.randomEmails = response.data.response
+            })
+        }
+    },
+    mounted() {
+        this.callApi()
+    }
+}).mount('#app')
